283f8bfe851f44bf5972e55c2951bfeb8387a336
Raye <brileyforbusiness@gmail.com>
2022-06-30 01:04:31 +0300

    File download added

--- client/src/store/reducers/filesReducer.ts
+++ client/src/store/reducers/filesReducer.ts
@@ -38,7 +38,7 @@ export const createDir = createAsyncThunk <IFile, {name: string, parent: string 
 	}
 )
 
-export const uploadFile = createAsyncThunk<IFile, {file: string, parent: string | null}, {rejectValue: string}>(
+export const uploadFile = createAsyncThunk<IFile, {file: File | string, parent: string | null}, {rejectValue: string}>(
 	'files/upload', async (data, {rejectWithValue}) => {
 		try {
 			const {file, parent} = data
@@ -70,6 +70,30 @@ export const uploadFile = createAsyncThunk<IFile, {file: string, parent: string 
 	}
 )
 
+export const downloadFile = createAsyncThunk<void, IFile, {rejectValue: string}>(
+	'files/download', async (file, {rejectWithValue}) => {
+		try {
+			const response = await fetch(`http://localhost:5000/api/files/download?id=${file._id}`,{
+				headers: {
+					authorization: `Bearer ${localStorage.getItem('token')}`
+				}
+			})
+			if (response.status === 200) {
+				const blob = await response.blob()
+				const downloadUrl = window.URL.createObjectURL(blob)
+				const link = document.createElement('a')
+				link.href = downloadUrl
+				link.download = file.name
+				document.body.appendChild(link)
+				link.click()
+				link.remove()
+			}
+		} catch (e: any) {
+			return rejectWithValue(e.response.data.message)
+		}
+	}
+)
+
 const initialState: FileState = {
 	files: [],
 	allFiles: [],
@@ -79,7 +103,11 @@ const initialState: FileState = {
 	],
 	error: '',
 	createPopupDisplay: 'none',
-	uploadPopupDisplay: 'none'
+	uploadPopupDisplay: 'none',
+	contextMenuType: '',
+	contextMenuFile: {
+		_id: '', type: '', name: '', user: '', path: '', size: 0, children: []
+	}
 }
 
 const filesSlice = createSlice({
@@ -100,6 +128,12 @@ const filesSlice = createSlice({
 		},
 		setUploadPopupDisplay(state, action) {
 			state.uploadPopupDisplay = action.payload
+		},
+		setContextMenuType(state, action) {
+			state.contextMenuType = action.payload
+		},
+		setContextMenuFile(state, action) {
+			state.contextMenuFile = action.payload
 		}
 	},
 	extraReducers: builder => {
@@ -117,8 +151,19 @@ const filesSlice = createSlice({
 			.addCase(uploadFile.fulfilled, (state, action) => {
 				state.files.push(action.payload)
 			})
+			.addCase(downloadFile.rejected, (state, action) => {
+				alert(action.payload)
+			})
 	}
 })
 
 export default filesSlice.reducer
-export const {setCurrentDir, pushToDirStack, sliceDirStack, setCreatePopupDisplay, setUploadPopupDisplay} = filesSlice.actions
+export const {
+	setCurrentDir,
+	pushToDirStack,
+	sliceDirStack,
+	setCreatePopupDisplay,
+	setUploadPopupDisplay,
+	setContextMenuType,
+	setContextMenuFile
+} = filesSlice.actions
--- client/src/types/file.ts
+++ client/src/types/file.ts
@@ -21,4 +21,6 @@ export interface FileState {
     error: string | undefined
     createPopupDisplay: string
     uploadPopupDisplay: string
+    contextMenuType: string
+    contextMenuFile: IFile
 }
